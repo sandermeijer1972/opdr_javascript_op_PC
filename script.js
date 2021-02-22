4bfa9ca96c967463933ed48e65036b6429ef78b7
sandermeijer1972 <69959339+sandermeijer1972@users.noreply.github.com>
2021-02-22 16:54:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Hallo Winc Academy studenten");
+// dit is een grote som
+/*let multiplication = 1230941 * 1823974;
+console.log('uitkomst vermenigvuldiging:', multiplication);
+let division = 637263 / 54;
+console.log('uitkomst deling:', division);*/
+console.log("vraag 4: nee, dat zie je niet, omdat het een comment is om het in je code te verduidelijken");
+console.log('vraag 6: nee, de uitkomsten zie je niet meer, omdat je ze omlijnd hebt met een comment');
